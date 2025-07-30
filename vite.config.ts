74dfdd629b46fcae1cbdf91c92f50164c3014f1c
Đặng Thông Nghĩa <141739914+dangthongnghia@users.noreply.github.com>
2025-07-30 16:32:46 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['music-app-nqq4.onrender.com']
+  }
+ 
 })
